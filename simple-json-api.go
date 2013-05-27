6b8174fa5b8884e83533afd27bd75f23dce2dc7a
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2013-05-27 13:19:42 -0400

    Marked-up word game (new game only)

---
+++ simple-json-api.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/coocood/jas"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+// STARTRECEIVESTRUCT OMIT
+// This will not actually be used, but it will be our reference
+// for what we expect the input data to look like
+type ReceivedData struct {
+	Id       int64  `json:"id"`
+	Name     string `json:"name"`
+	Guess    string `json:"guess"`
+	NextChar string `json:"nextchar"`
+}
+// ENDRECEIVESTRUCT OMIT
+
+func sigintCatcher() {
+	ch := make(chan os.Signal)
+	signal.Notify(ch, syscall.SIGINT)
+	<-ch
+	os.Exit(0)
+}
+
+func main() {
+	// Catch signals
+	go sigintCatcher()
+
+	router := jas.NewRouter(new(Wordgame))
+	router.RequestErrorLogger = router.InternalErrorLogger
+
+	log.Println("Starting serving on paths:\n" + router.HandledPaths(true))
+	http.Handle(router.BasePath, router)
+	err := http.ListenAndServe("127.0.0.1:8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
