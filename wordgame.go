77ce255f09e3d941addff7f5b6f688f967b8a9a3
Jeff Mitchell <jeffrey.mitchell@gmail.com>
2013-06-02 09:13:47 -0400

    Fix up panics

--- wordgame.go
+++ wordgame.go
@@ -48,13 +48,13 @@ func (*Wordgame) GetNewgame(ctx *jas.Context) {
 
 	if err != nil {
 		ctx.Error = jas.NewInternalError("Got an error generating a game ID")
-		panic(nil)
+		panic(ctx.Error)
 	}
 
 	name, err := ctx.FindString("name")
 	if err != nil {
 		ctx.Error = jas.NewRequestError("Player's name must be provided")
-		panic(nil)
+		panic(ctx.Error)
 	}
 
 	currgame := randint.Int64()
@@ -114,7 +114,7 @@ func (*Wordgame) PostGuess(ctx *jas.Context) {
 	var ok bool
 	if game, ok = games[id]; !ok {
 		ctx.Error = jas.NewRequestError("Given game ID not found")
-		panic(nil)
+		panic(ctx.Error)
 	}
 	if len(nextchar) != 1 {
 		//FIXME
